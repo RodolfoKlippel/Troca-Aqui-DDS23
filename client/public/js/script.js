572f5f84442760ef68c9dbef4057fae46f945376
Rodolfo Klippel <dolforr@gmail.com>
2026-05-18 17:14:42 -0300

    quarto commit

---
+++ client/public/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const inputFoto = document.getElementById("foto")
+    const previewBox = document.getElementById("preview")
+    const previewImg = document.getElementById("preview-img")
+
+    if(inputFoto && previewBox && previewImg){
+        if(!previewImg.getAttribute("src") ||
+            previewImg.getAttribute("src") ==="..." ||
+            previewImg.getAttribute("src") === ""){
+                previewImg = "/img/sem-foto.png"
+                previewBox.style.display = "block"
+        }
+
+        inputFoto.addEventListener("change", function(evento){
+            
+            const arquivo = evento.target.files[0]
+
+            if(arquivo){
+                const leitorDeArquivo = new FileReader()
+
+                leitorDeArquivo.onload = function(e){
+                    previewImg.src = e.target.result
+                    previewBox.style.display = "block"
+                }
+
+                leitorDeArquivo.readAsDataURL(arquivo)
+            }else{
+                previewImg.src = "/img/sem-foto.png"
+                previewBox.style.display = "block"
+            }
+        })
+    }
+})
